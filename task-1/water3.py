aabc631fb45fbe08b94c7312776d025f258d0ae7
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2023-09-09 16:39:43 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,33 +1,24 @@
 def initial_state():
     return (8, 0, 0)
 
+
 def is_goal(s):
-    return s[0] == 4 and s[1] == 4
+    return s == (4, 4, 0)
 
 
 def successors(s):
-    x, y, z = s
 
-    # x -> y
-    t = min(5-y, x)
-    if t != 0: yield ((x-t, y+t, z), t)
+    capacity = (8, 5, 3)
 
-    # x -> z
-    t = min(3-z, x)
-    if t != 0: yield ((x-t, y, z+t), t)
+    for i in range(3):
+        for j in range(3):
 
-    # y -> x
-    t = min(8-x, y)
-    if t != 0: yield ((x+t, y-t, z), t)
+            if i == j: continue
 
-    # y -> z
-    t = min(3-z, y)
-    if t != 0: yield ((x, y-t, z+t), t)
+            t = min(s[i], capacity[j] - s[j])
+            if t == 0: continue
 
-    # z -> x
-    t = min(8-x, z)
-    if t != 0: yield ((x+t, y, z-t), t)
+            d = [0, 0, 0]
+            d[i], d[j] = -t, t
 
-    # z -> y
-    t = min(5-y, z)
-    if t != 0: yield ((x, y+t, z-t), t)
+            yield ((s[0] + d[0], s[1] + d[1], s[2] + d[2]), t)
